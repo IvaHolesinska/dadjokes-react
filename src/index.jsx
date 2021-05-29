75100d02b4bc7a0fbb64e5b6a4f47deb98b975fa
Iva Holešinská <holesinska.iva@gmail.com>
2021-05-29 15:18:19 +0200

    task 15

--- src/index.jsx
+++ src/index.jsx
@@ -1,56 +1,27 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { render } from 'react-dom';
+import { Joke, jokes } from './Joke/Joke';
 
 import './style.css';
 
 const App = () => {
-  const [like, setLike] = useState(0);
-  const [dislike, setDislike] = useState(0);
-
   return (
-    <div className="container">
+    <>
       <div className="container">
-        <div className="joke">
-          <div className="joke__body">
-            <div className="joke__user">
-              <img
-                className="user-avatar"
-                src="https://raw.githubusercontent.com/Czechitas-podklady-WEB/dadjokes/main/users/user01.png"
-              />
-              <p className="user-name">Neroxx</p>
-            </div>
-
-            <p className="joke__text">
-              The secret service isn't allowed to yell "Get down!" anymore when
-              the president is about to be attacked. Now they have to yell
-              "Donald, duck!"
-            </p>
-          </div>
-          <div className="joke__likes">
-            <button
-              id="btn-up"
-              className="btn-like btn-like--up"
-              onClick={() => {
-                setLike(like + 1);
-              }}
-            ></button>
-            <span id="likes-up" className="likes-count likes-count--up">
-              {like}
-            </span>
-            <button
-              id="btn-down"
-              className="btn-like btn-like--down"
-              onClick={() => {
-                setDislike(dislike + 1);
-              }}
-            ></button>
-            <span id="likes-down" className="likes-count likes-count--down">
-              {dislike}
-            </span>
-          </div>
+        <div className="container">
+          {jokes.map((joke) => (
+            <Joke
+              key={joke.id}
+              userAvatar={joke.avatar}
+              userName={joke.name}
+              text={joke.text}
+              likes={joke.likes}
+              dislikes={joke.dislikes}
+            />
+          ))}
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
